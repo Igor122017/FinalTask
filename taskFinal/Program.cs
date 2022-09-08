d9c3f6d21ca1fb40849ba94c6a925b40f2e0425b
«George» <prior21@mail.ru>
2022-09-08 18:17:04 +0300

    add c# program

---
+++ taskFinal/Program.cs
@@ -0,0 +1,24 @@
+﻿void SortArray(string[] array, string[] secondArray)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            secondArray[i] = array[i];
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == string.Empty) i++;
+        else Console.Write($"{array[i]} ");
+    }
+}
+Console.Clear();
+string[] array = { "hello", "2", "world", ":-)" };
+string[] newarray = new string[array.Length];
+SortArray(array, newarray);
+PrintArray(newarray);
